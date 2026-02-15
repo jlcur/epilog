1e7d4701c8fc28fb10671c49634aaa4a2c707de9
jlcur <jordan@jordancurley.com>
2026-02-15 16:28:43 +0000

    Add script for migrating test database to latest migration file

---
+++ src/tests/utils/migrate-db.ts
@@ -0,0 +1,34 @@
+import { promises as fs } from "node:fs";
+import * as path from "node:path";
+import { FileMigrationProvider, Migrator } from "kysely";
+import { db } from "../../shared/database/database.ts";
+
+export async function migrateDb() {
+	const migrator = new Migrator({
+		db,
+		provider: new FileMigrationProvider({
+			fs,
+			path,
+			migrationFolder: path.join(
+				import.meta.dirname,
+				"../../shared/database/migrations",
+			),
+		}),
+	});
+
+	const { error, results } = await migrator.migrateToLatest();
+
+	results?.forEach((it) => {
+		if (it.status === "Success") {
+			console.log(`migration "${it.migrationName}" was executed successfully`);
+		} else if (it.status === "Error") {
+			console.error(`failed to execute migration "${it.migrationName}"`);
+		}
+	});
+
+	if (error) {
+		console.error("failed to migrate");
+		console.error(error);
+		process.exit(1);
+	}
+}
