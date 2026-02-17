0f13c6c3e6d70489b9972a4ab7b924a0d3a8ff94
jlcur <jordan@jordancurley.com>
2026-02-17 16:47:44 +0000

    Create comments table migration file

---
+++ src/shared/database/migrations/1771168649210_create_comments_table.ts
@@ -0,0 +1,20 @@
+import { type Kysely, sql } from "kysely";
+
+// `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
+export async function up(db: Kysely<any>): Promise<void> {
+	await db.schema
+		.createTable("comments")
+		.addColumn("id", "uuid", (col) =>
+			col.primaryKey().defaultTo(sql`gen_random_uuid()`),
+		)
+		.addColumn("content", "text", (col) => col.notNull())
+		.addColumn("created_at", "timestamptz", (col) =>
+			col.defaultTo(sql`now()`).notNull(),
+		)
+		.execute();
+}
+
+// `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
+export async function down(db: Kysely<any>): Promise<void> {
+	await db.schema.dropTable("comments").execute();
+}
