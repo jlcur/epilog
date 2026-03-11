4b5831027d64f774717ca8e47fbadc1f982e233c
jlcur <jordan@jordancurley.com>
2026-03-11 14:57:39 +0000

    Add new auth table types

--- src/shared/database/types.ts
+++ src/shared/database/types.ts
@@ -9,6 +9,55 @@ export interface CommentTable {
 	is_deleted: Generated<boolean>;
 }
 
+interface UserTable {
+	id: string;
+	name: string;
+	email: string;
+	emailVerified: boolean;
+	image?: string | null;
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+interface SessionTable {
+	id: string;
+	userId: string;
+	token: string;
+	expiresAt: Date;
+	ipAddress?: string | null;
+	userAgent?: string | null;
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+interface AccountTable {
+	id: string;
+	userId: string;
+	accountId: string;
+	providerId: string;
+	accessToken?: string | null;
+	refreshToken?: string | null;
+	accessTokenExpiresAt?: Date | null;
+	refreshTokenExpiresAt?: Date | null;
+	scope?: string | null;
+	password?: string | null;
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+interface VerificationTable {
+	id: string;
+	identifier: string;
+	value: string;
+	expiresAt: Date;
+	createdAt?: Date | null;
+	updatedAt?: Date | null;
+}
+
 export interface Database {
 	comments: CommentTable;
+	user: UserTable;
+	session: SessionTable;
+	account: AccountTable;
+	verification: VerificationTable;
 }
