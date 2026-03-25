62f52b0a6dacfa17cd83c285652419923e2a9af3
jlcur <jordan@jordancurley.com>
2026-03-25 10:34:39 +0000

    Add migration script to run before server starts

---
+++ src/shared/database/migrate.ts
@@ -0,0 +1,34 @@
+import fs from "node:fs/promises";
+import * as path from "node:path";
+import { fileURLToPath } from "node:url";
+import { FileMigrationProvider, Migrator } from "kysely";
+import { db } from "./database.ts";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
+export async function migrateToLatest() {
+	const migrator = new Migrator({
+		db,
+		provider: new FileMigrationProvider({
+			fs,
+			path,
+			migrationFolder: path.join(__dirname, "migrations"),
+		}),
+	});
+
+	const { error, results } = await migrator.migrateToLatest();
+
+	results?.forEach((it) => {
+		if (it.status === "Success") {
+			console.log(`migration "${it.migrationName}" was executed successfully`);
+		} else if (it.status === "Error") {
+			console.error(`failed to execute migration "${it.migrationName}"`);
+		}
+	});
+
+	if (error) {
+		console.error("failed to migrate");
+		console.error(error);
+		process.exit(1);
+	}
+}
