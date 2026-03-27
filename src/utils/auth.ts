afa00e7cd7aadd0560290312ad4af6af2e06ff20
jlcur <jordan@jordancurley.com>
2026-03-27 11:37:40 +0000

    Set base auth path in config

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -2,11 +2,15 @@ import { betterAuth } from "better-auth";
 import { admin } from "better-auth/plugins";
 import { db } from "../shared/database/database.ts";
 
+const prefix = process.env.API_PREFIX || "/api/v1";
+
 export const auth = betterAuth({
 	database: {
 		db: db,
 		type: "postgres",
 	},
+
+	basePath: `${prefix}/auth`,
 
 	emailAndPassword: {
 		enabled: true,
